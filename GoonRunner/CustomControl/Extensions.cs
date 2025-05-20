f59053af9e921d19c6a8912d174bf3647fe0d163
El Vinlos <vuongkhanhvinh205@gmail.com>
2025-05-20 16:52:07 +0700

    Update Listview

--- GoonRunner/CustomControl/Extensions.cs
+++ GoonRunner/CustomControl/Extensions.cs
@@ -1,11 +1,14 @@
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace GoonRunner.CustomControl
 {
     internal class Extensions
     {
         public static readonly DependencyProperty Header =
-            DependencyProperty.RegisterAttached("Header", typeof(string), typeof(Extensions), new PropertyMetadata(default(string)));
+            DependencyProperty.RegisterAttached("Header", typeof(string), typeof(Extensions),
+                new PropertyMetadata(default(string)));
 
         public static void SetHeader(UIElement element, string value)
         {
@@ -17,9 +20,10 @@ namespace GoonRunner.CustomControl
         {
             return (string)element.GetValue(Header);
         }
-        
+
         public static readonly DependencyProperty Icon =
-            DependencyProperty.RegisterAttached("Icon", typeof(string), typeof(Extensions), new PropertyMetadata(default(string)));
+            DependencyProperty.RegisterAttached("Icon", typeof(string), typeof(Extensions),
+                new PropertyMetadata(default(string)));
 
         public static void SetIcon(UIElement icon, string value)
         {
@@ -30,6 +34,38 @@ namespace GoonRunner.CustomControl
         {
             return (string)icon.GetValue(Icon);
         }
-        
+
+
+        // Tạo ra một DateTemplate tại vì sử dụng nó trong style ko được
+        // yes, t biết nó lỏ nhưng chịu    
+        public static readonly DependencyProperty BindingProperty =
+            DependencyProperty.RegisterAttached(
+                "Binding",
+                typeof(string),
+                typeof(Extensions),
+                new PropertyMetadata(null, OnBindingChanged));
+
+        public static void SetBinding(DependencyObject element, string value)
+            => element.SetValue(BindingProperty, value);
+
+        public static string GetBinding(DependencyObject element)
+            => (string)element.GetValue(BindingProperty);
+
+        private static void OnBindingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is GridViewColumn column && e.NewValue is string bindingPath)
+            {
+                var textBlockFactory = new FrameworkElementFactory(typeof(TextBlock));
+                textBlockFactory.SetBinding(TextBlock.TextProperty, new Binding(bindingPath));
+                textBlockFactory.SetValue(TextBlock.HorizontalAlignmentProperty, HorizontalAlignment.Center);
+                textBlockFactory.SetValue(TextBlock.VerticalAlignmentProperty, VerticalAlignment.Center);
+
+                // Optional: Apply style
+                textBlockFactory.SetResourceReference(FrameworkElement.StyleProperty, "CenteredGridViewCellStyle");
+
+                var template = new DataTemplate { VisualTree = textBlockFactory };
+                column.CellTemplate = template;
+            }
+        }
     }
 }
