7b0aa09bbb71cc18fc4445b6a42ab189fd052bb0
El Vinlos <vuongkhanhvinh205@gmail.com>
2025-05-20 17:00:28 +0700

    Fix placeholder text cho password

--- GoonRunner/MVVM/ViewModel/ForgotPasswordViewModel.cs
+++ GoonRunner/MVVM/ViewModel/ForgotPasswordViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using GoonRunner.MVVM.View;
 
@@ -11,7 +12,12 @@ namespace GoonRunner.MVVM.ViewModel
 {
     public class ForgotPasswordViewModel : BaseViewModel
     {
+        private string _password;
+        public string Password { get => _password; set { _password = value; OnPropertyChanged(); } }
+        private string _placeholder = "Nhập mật khẩu mới";
+        public string Placeholder { get => _placeholder; set { _placeholder = value; OnPropertyChanged(); } }
         public ICommand ReturnCommand { get; set; }
+        public ICommand PasswordChangedCommand { get; set; }
         public ForgotPasswordViewModel()
         {
             ReturnCommand = new RelayCommand<Window>(p =>
@@ -19,6 +25,11 @@ namespace GoonRunner.MVVM.ViewModel
                 LogInView loginView = new LogInView();
                 loginView.Show();
                 p.Hide();
+            });
+            PasswordChangedCommand = new RelayCommand<PasswordBox>((p) => true, (p) =>
+            {
+                Placeholder = string.IsNullOrEmpty(p.Password) ? "Nhập mật khẩu mới" : "";
+                Password = p.Password;
             });
         }
     }
