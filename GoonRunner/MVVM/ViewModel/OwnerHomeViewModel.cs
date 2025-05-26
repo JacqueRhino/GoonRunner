f24fa3bef5b9fe94c137ac4508c636d109dd929a
El-Vinlos <vuongkhanhvinh205@gmail.com>
2025-05-26 23:28:57 +0700

    Thêm lợi nhuận theo tháng
    * update ui
    
    * Update Lịch làm
    
    * update solidcolorbrush
    
    * Thêm lợi nhuận theo tháng

--- GoonRunner/MVVM/ViewModel/OwnerHomeViewModel.cs
+++ GoonRunner/MVVM/ViewModel/OwnerHomeViewModel.cs
@@ -1,11 +1,7 @@
 ﻿using GoonRunner.MVVM.Model;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -15,20 +11,34 @@ namespace GoonRunner.MVVM.ViewModel
     {
         private ObservableCollection<DoanhThuTheoNgay> _doanhthutheongaylist;
         public ObservableCollection<DoanhThuTheoNgay> DoanhThuTheoNgayList { get { return _doanhthutheongaylist; } set { _doanhthutheongaylist = value; OnPropertyChanged(); } }
+        private ObservableCollection<DoanhThuTheoNgay> _loinhuantheothang;
+        public ObservableCollection<DoanhThuTheoNgay> LoiNhuanTheoThangList { get { return _loinhuantheothang; } set { _loinhuantheothang = value; OnPropertyChanged(); } }
         private DateTime _selecteddate;
         public DateTime SelectedDate { get => _selecteddate; set { if (_selecteddate != value) { _selecteddate = value; HienThiDoanhThu = 0; LoadDoanhThu(); OnPropertyChanged(); } } }
         private int _ngaychon;
-        public int NgayChon { get => _ngaychon; set { _ngaychon = SelectedDate.Day; OnPropertyChanged(); } }
+        public int NgayChon { get => _ngaychon; set { _ngaychon = value; _ngaychon = SelectedDate.Day; OnPropertyChanged(); } }
         private int _hienthidoanhthu;
         public int HienThiDoanhThu { get => _hienthidoanhthu; set { _hienthidoanhthu = value; OnPropertyChanged(); } }
+        private int _hienthiloinhuan;
+        public int HienThiLoiNhuan { get => _hienthiloinhuan; set { _hienthiloinhuan = value; OnPropertyChanged(); } }
         public ICommand RefreshCommand { get; set; }
         public OwnerHomeViewModel()
         {
             HienThiDoanhThu = 0;
+            HienThiLoiNhuan = 0;
             SelectedDate = DateTime.Today;
+            LoadLoiNhuan();
             LoadDoanhThu();
-            RefreshCommand = new RelayCommand<Button>((p) => true, (p) => { LoadDoanhThu(); });
+            RefreshCommand = new RelayCommand<Button>((p) => true, (p) => { RefreshView();});
         }
+
+        private void RefreshView()
+        {
+            LoadDoanhThu();
+            LoadLoiNhuan();
+        }
+
+
         private void LoadDoanhThu()
         {
             DoanhThuTheoNgayList = new ObservableCollection<DoanhThuTheoNgay>();
@@ -36,20 +46,28 @@ namespace GoonRunner.MVVM.ViewModel
             int month = SelectedDate.Month;
             int day = SelectedDate.Day;
 
-            var DoanhThu = DataProvider.Ins.goonRunnerDB.DoanhThuTheoNgays.Where(n => n.Ngay == day && n.Thang == month && n.Nam == year && n.TongDoanhThu > 0).ToList();
-            foreach (var item in DoanhThu)
+            var doanhThu = DataProvider.Ins.goonRunnerDB.DoanhThuTheoNgays.Where(n => n.Ngay == day && n.Thang == month && n.Nam == year && n.TongDoanhThu > 0).ToList();
+            foreach (var _ in doanhThu)
             {
                 DoanhThuTheoNgay doanhthutheongay = new DoanhThuTheoNgay();
-                if (DoanhThu != null)
-                {
-                    HienThiDoanhThu = DoanhThu.Sum(n => n.TongDoanhThu) ?? 0;
-                }
-                else
-                {
-                    HienThiDoanhThu = 0;
-                }
+                HienThiDoanhThu = doanhThu.Sum(n => n.TongDoanhThu) ?? 0;
                 DoanhThuTheoNgayList.Add(doanhthutheongay);
             }
+        }
+        
+        private void LoadLoiNhuan()
+        {
+            LoiNhuanTheoThangList = new ObservableCollection<DoanhThuTheoNgay>();
+            var year = DateTime.Now.Year;
+            var month = DateTime.Now.Month;
+            var loinhuan = DataProvider.Ins.goonRunnerDB.DoanhThuTheoNgays.Where(n => n.Thang == month && n.Nam == year && n.LoiNhuan > 0).ToList();
+            foreach (var _ in loinhuan)
+            {
+                DoanhThuTheoNgay loinhuantheothang = new DoanhThuTheoNgay();
+                HienThiLoiNhuan = loinhuan.Sum(n => n.LoiNhuan) ?? 0;
+                LoiNhuanTheoThangList.Add(loinhuantheothang);
+
+            } 
         }
     }
 }
