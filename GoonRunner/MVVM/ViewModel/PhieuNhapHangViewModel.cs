78682350b93c200ef8caae7c29ed82233bdf97a2
El Vinlos <vuongkhanhvinh205@gmail.com>
2025-05-27 21:17:56 +0700

    Xong thanh tìm kiếm cho các view

--- GoonRunner/MVVM/ViewModel/PhieuNhapHangViewModel.cs
+++ GoonRunner/MVVM/ViewModel/PhieuNhapHangViewModel.cs
@@ -1,8 +1,11 @@
-﻿using GoonRunner.MVVM.Model;
+﻿using System;
+using GoonRunner.MVVM.Model;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 
 namespace GoonRunner.MVVM.ViewModel
@@ -13,12 +16,22 @@ namespace GoonRunner.MVVM.ViewModel
         public ObservableCollection<PHIEUNHAPHANG> PhieuNhapHangList { get { return _phieunhaphanglist; } set { _phieunhaphanglist = value; OnPropertyChanged(); } }
         private PHIEUNHAPHANG _selectedItem;
         public PHIEUNHAPHANG SelectedItem { get => _selectedItem; set { _selectedItem = value; OnPropertyChanged(); } }
+        private string _filterText;
+        public string FilterText { get => _filterText; set { _filterText = value; OnPropertyChanged(); FilteredPhieuNhapHangList.Refresh(); } }
+        private ICollectionView _filteredphieunhaphanglist;
+        public ICollectionView FilteredPhieuNhapHangList { get => _filteredphieunhaphanglist; set { _filteredphieunhaphanglist = value; OnPropertyChanged(); } }
         public ICommand DoubleClickCommand { get; set; }
         public ICommand RefreshCommand { get; set; }
         public PhieuNhapHangViewModel()
         {
             LoadPhieuNhapHangList();
-            RefreshCommand = new RelayCommand<Button>((p) => true, (p) => { LoadPhieuNhapHangList(); });
+            RefreshCommand = new RelayCommand<Button>((p) => true, (p) =>
+            {
+                LoadPhieuNhapHangList();
+                FilterText = string.Empty;
+            });
+            FilteredPhieuNhapHangList = CollectionViewSource.GetDefaultView(PhieuNhapHangList);
+            FilteredPhieuNhapHangList.Filter = FilterPhieuNhapHang;
             DoubleClickCommand = new RelayCommand<object>((p) => SelectedItem != null, (p) =>
             {
                 MainViewModel.Instance.ChiTietPhieuNhapHangVM = new ChiTietPhieuNhapHangViewModel(SelectedItem.MaPNH);
@@ -32,7 +45,6 @@ namespace GoonRunner.MVVM.ViewModel
         {
             PhieuNhapHangList = new ObservableCollection<PHIEUNHAPHANG>();
             var DanhSachPhieuNhapHang = DataProvider.Ins.goonRunnerDB.PHIEUNHAPHANGs;
-            int i = 1;
             foreach (var item in DanhSachPhieuNhapHang)
             {
                 PHIEUNHAPHANG phieunhaphang = new PHIEUNHAPHANG();
@@ -42,8 +54,30 @@ namespace GoonRunner.MVVM.ViewModel
                 phieunhaphang.NgayNhap = item.NgayNhap;
                 phieunhaphang.MaNV = item.MaNV;
                 PhieuNhapHangList.Add(phieunhaphang);
-                i++;
             }
+        }
+        
+        private bool FilterPhieuNhapHang(object item)
+        {
+            if (string.IsNullOrWhiteSpace(FilterText))
+                return true;
+
+            if (!(item is PHIEUNHAPHANG phieunhaphang))
+            {
+                return false;
+            }
+
+            if (int.TryParse(FilterText, out var maPNH ))
+            {
+                if (phieunhaphang.MaPNH == maPNH)
+                    return true;
+            }
+            
+            if ( phieunhaphang.TenNCC?.IndexOf(FilterText, StringComparison.OrdinalIgnoreCase) >= 0)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
