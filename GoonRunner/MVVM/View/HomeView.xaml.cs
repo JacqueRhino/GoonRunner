207b0d78b48a5c7b0ad5689261bf338c600a3c81
El Vinlos <vuongkhanhvinh205@gmail.com>
2025-10-05 04:30:48 +0700

    make homeview topbar responsive

--- GoonRunner/MVVM/View/HomeView.xaml.cs
+++ GoonRunner/MVVM/View/HomeView.xaml.cs
@@ -1,12 +1,41 @@
-using System.Windows.Controls;
+using System.Windows;
 
 namespace GoonRunner.MVVM.View
 {
-    public partial class HomeView : UserControl
+    public partial class HomeView
     {
         public HomeView()
         {
             InitializeComponent();
+            Loaded += HomeView_Loaded;
+            SizeChanged += HomeView_SizeChanged;
+        }
+
+        private void HomeView_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateLayoutForWidth(ActualWidth);
+        }
+
+        private void HomeView_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateLayoutForWidth(e.NewSize.Width);
+        }
+
+        private void UpdateLayoutForWidth(double width)
+        {
+            if (TopStatsPanel == null) return;
+
+            switch (width)
+            {
+                case < 935:
+                    TopStatsPanel.Columns = 2;
+                    TopStatsPanel.Rows = 2;
+                    break;
+                default:
+                    TopStatsPanel.Columns = 4;
+                    TopStatsPanel.Rows = 1;
+                    break;
+            }
         }
     }
 }
